70d5f64a1e61c2e3296fdef5a79dd7dc6e46ae18
Patrick Gatewood <PatrickGatewood@TARDIS.local>
2017-04-11 14:07:27 -0400

    Adds 'add new date' front-end functionality; minor CSS tweaks

--- group/src/java/com/mycompany/jsfClasses/MyMeetingsController.java
+++ group/src/java/com/mycompany/jsfClasses/MyMeetingsController.java
@@ -7,12 +7,16 @@ package com.mycompany.jsfClasses;
 import com.mycompany.entityClasses.Meeting;
 import com.mycompany.entityClasses.User;
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.ejb.EJBException;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
+import org.primefaces.event.SelectEvent;
 
 @Named("myMeetingsController")
 @SessionScoped
@@ -23,23 +27,58 @@ import javax.faces.context.FacesContext;
  * @author PatrickGatewood
  */
 public class MyMeetingsController implements Serializable {
-    
+
     @EJB
     private com.mycompany.sessionBeans.UserFacade userFacade;
     private User loggedInUser;
+    private Date newDate;
+    private String newDateString;
+    
+    public MyMeetingsController() {
+        newDateString = new String();
+    }
 
     /**
      * Gets the object representing the logged in user.
-     *
-     * @param username the username to search
-     * @return
+     * 
+     * @return the user currently logged in
      */
     public User getLoggedInUser() {
         loggedInUser = userFacade.findByUsername((String) FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("username"));
         return loggedInUser;
     }
 
+    public void onDateSelect(SelectEvent event) {
+//        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+//        newDateString = format.format(event.getObject());
+    }
+
     public List<Meeting> getLoggedInUserMeetings() {
         return (List<Meeting>) loggedInUser.getMeetingCollection();
     }
+
+    public Date getNewDate() {
+        return newDate;
+    }
+
+    public void setNewDate(Date newDate) {
+        this.newDate = newDate;
+    }
+
+    public String getNewDateString() {
+        return newDateString;
+    }
+
+    public void setNewDateString(String newDateString) {
+        this.newDateString = newDateString;
+    }
+    
+    
+    
+  
+    
+    
+    
+    
+
 }
