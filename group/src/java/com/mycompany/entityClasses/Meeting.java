53b2cf8d525298585a1d954b54f275966cbed6d9
Alex Martin <alexander.martin@excella.com>
2017-04-13 13:07:26 -0400

    Fixed merge issue

--- group/src/java/com/mycompany/entityClasses/Meeting.java
+++ group/src/java/com/mycompany/entityClasses/Meeting.java
@@ -7,6 +7,7 @@ package com.mycompany.entityClasses;
 
 import java.io.Serializable;
 import java.util.Collection;
+import java.util.List;
 import javax.persistence.Basic;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
