0a7c371511d2a112ca681037d3987394eb4afc95
Erin Kocis <erink13@vt.edu>
2017-04-16 17:58:55 -0400

    beginnings of email stuff

---
+++ group/src/java/com/mycompany/validators/EmailAddressValidator.java
@@ -0,0 +1,67 @@
+/*
+ * Created by Erin Kocis on 2017.04.14  * 
+ * Copyright © 2017 Erin Kocis. All rights reserved. * 
+ */
+package com.mycompany.validators;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.FacesValidator;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
+/**
+ *
+ * @author Erin
+ */
+
+/*
+The @FacesValidator annotation registers this class with the runtime as a validator-class.
+The value attribute "emailAddressValidator" is set to be the validator-id.
+A JSF XHTML page uses this id to execute the "validate" method of this class.
+For example, the SendEmail.xhtml page uses <f:validator validatorId="emailAddressValidator" /> 
+to execute the "validate" method of this class.
+ */
+@FacesValidator("emailAddressValidator")
+public class EmailAddressValidator implements Validator {
+
+    @Override
+    public void validate(FacesContext context, UIComponent component, Object value) throws ValidatorException {
+
+        // Type cast the inputted "value" to email of type String
+        String email = (String) value;
+
+        if (email == null || email.isEmpty()) {
+            // Do not take any action. 
+            // The required="true" in the XHTML file will catch this and produce an error message.
+            return;
+        }
+
+        /*
+            ^                   # start of the regex line
+            [_A-Za-z0-9-\\+]+   # can start with underscore, A to Z, a to z, 0 to 9, hyphen, and can contain one or more "+"
+            (                   # start of group 1
+            \\.[_A-Za-z0-9-]+   # after a dot "." it can start with underscore, A to Z, a to z, 0 to 9, or hyphen
+            )*                  # end of group1. "*" designates this group as optional.
+            @                   # must contain the "@" symbol
+            [A-Za-z0-9-]+       # after the "@" symbol, it can start with A to Z, a to z, 0 to 9, or hyphen
+            (                   # start of group 2
+            \\.[A-Za-z0-9]+     # after a dot "." it can start with A to Z, a to z, or 0 to 9
+            )*                  # end of group 2. "*" designates this group as optional.
+            (                   # start of group 3
+            \\.[A-Za-z]{2,}     # after a dot "." it can start with A to Z or a to z, with minimum length of 2
+            )                   # end of group #3
+            $                   # end of the regex line
+         */
+        // REGular EXpression (regex) to validate the email address entered
+        String regex = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+        if (!email.matches(regex)) {
+            throw new ValidatorException(new FacesMessage("Please Enter a Valid Email Address!"));
+        }
+    }
+
+}
+
